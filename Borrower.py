e4abdf48a96d36a19cd1c2a9dd6608b20a646cc0
Ibrahim El-Mokhtar <ibrahimelmokhtar@gmail.com>
2021-02-03 22:07:32 +0200

    Implement Borrower() class

---
+++ Borrower.py
@@ -0,0 +1,50 @@
+from Member import *
+
+class Borrower(Member):
+    def __init__(self, id, name, address, isbn, borrow_date, return_date):
+        """Create a new borrower.
+
+        Args:
+            id (string): Member's ID.
+            name (string): Member's name.
+            address (string): Member's address.
+            isbn (string): Book's ISBN.
+            borrow_date (date): Actual borrowing date.
+            return_date (date): Expected return date.
+        """
+        super().__init__(id, name, address)
+        self.isbn = isbn
+        self.borrow_date = borrow_date
+        self.return_date = return_date
+
+    def __str__(self):
+        """ Print the Borrower object to the terminal.
+        """
+
+        output = """
+        Borrower's Info.
+        \tID\t\t: {}
+        \tName\t\t: {}
+        \tAddress\t\t: {}
+        \tBook ISBN\t: {}
+        \tBorrowing Date\t: {}
+        \tReturn Date\t: {}
+        """
+
+        return output.format(self.id, self.name, self.address,
+                             self.isbn, self.borrow_date, self.return_date)
+
+# Setter and Getter functions:
+    def setISBN(self, isbn):
+        self.isbn = isbn
+    def setBorrowDate(self, borrow_date):
+        self.borrow_date = borrow_date
+    def setReturnDate(self, return_date):
+        self.return_date = return_date
+
+    def getISBN(self):
+        return self.isbn
+    def getBorrowDate(self):
+        return self.borrow_date
+    def getReturnDate(self):
+        return self.return_date
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from Book import *
 from Member import *
+from Borrower import *
 
-m1 = Member("14p8195", "Ibrahim", "Cairo, Egypt")
-print(m1)
+borrow_date = datetime.datetime(2020, 11, 7).date()
+return_date = datetime.datetime(2021, 1, 7).date()
+
+b1 = Borrower("14p8195", "Ibrahim", "Cairo, Egypt", "12345", borrow_date, return_date)
+print(b1)
